6eed27e58eb6003900ced3ac0eddea4a17c4ea13
maxicasella <maxicasella@gmail.com>
2023-11-11 11:12:21 -0300

    Initial commit

---
+++ Assets/Matrix.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class Matrix<T> : IEnumerable<T>
+{
+    //IMPLEMENTAR: ESTRUCTURA INTERNA- DONDE GUARDO LOS DATOS?
+
+    public Matrix(int width, int height)
+    {
+        //IMPLEMENTAR: constructor
+    }
+
+	public Matrix(T[,] copyFrom)
+    {
+        //IMPLEMENTAR: crea una version de Matrix a partir de una matriz básica de C#
+    }
+
+	public Matrix<T> Clone() {
+        Matrix<T> aux = new Matrix<T>(Width, Height);
+        //IMPLEMENTAR
+        return aux;
+    }
+
+	public void SetRangeTo(int x0, int y0, int x1, int y1, T item) {
+        //IMPLEMENTAR: iguala todo el rango pasado por parámetro a item
+    }
+
+    //Todos los parametros son INCLUYENTES
+    public List<T> GetRange(int x0, int y0, int x1, int y1) {
+        List<T> l = new List<T>();
+        //IMPLEMENTAR
+        return l;
+	}
+
+    //Para poder igualar valores en la matrix a algo
+    public T this[int x, int y] {
+		get
+        {
+            //IMPLEMENTAR
+            return default(T);
+		}
+		set {
+            //IMPLEMENTAR
+		}
+	}
+
+    public int Width { get; private set; }
+
+    public int Height { get; private set; }
+
+    public int Capacity { get; private set; }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        //IMPLEMENTAR
+        yield return default(T);
+    }
+
+	IEnumerator IEnumerable.GetEnumerator() {
+		return GetEnumerator();
+	}
+}
